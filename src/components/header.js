eb79bdfec17310cd8f0465917f96f67f90458aaf
Eve Mendelevich <ym9382@gmail.com>
2020-06-15 20:35:57 -0400

    updated site metadata & removed unnecessary images/links

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `hotpink`,
+      background: `black`,
       marginBottom: `1.45rem`,
     }}
   >
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,16 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Welcome to my personal site!</h1>
+    <p>I'm building it with Gatsby.js.</p>
+    <p>
+      It's currently a work in progress, but my projects will appear here
+      sometime in the future.
+    </p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      {/* <Image /> */}
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    {/* <Link to="/page-2/">Go to page 2</Link> <br /> */}
   </Layout>
 )
 

091b06c86cb32948970a42e70fe887e7a0885b8c
Eve Mendelevich <ym9382@gmail.com>
2020-07-06 16:06:13 -0400

    added empty projects section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,18 +4,22 @@ import Image from "./image"
 
 const Hero = () => {
   return (
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `0 1.0875rem 1.45rem`,
-        height: '100vh',
-        backgroundColor: 'var(--test-9)',
-      }}
-    >
-      <h1>Welcome to my personal site!</h1>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
+    <div style={{
+      height: '100vh',
+      backgroundColor: 'var(--test-9)',
+      paddingTop: '5rem'
+    }}>
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`,
+        }}
+      >
+        <h1>Welcome to my personal site!</h1>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
       </div>
     </div>
   )
--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ import headerStyles from './header.module.css'
 const Header = ({ siteTitle }) => (
   <header style={{
     background: 'var(--test-7)',
-    marginBottom: "1.45rem",
+    // marginBottom: "1.45rem",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
@@ -15,7 +15,9 @@ const Header = ({ siteTitle }) => (
     top: 0,
     left: 0,
     right: 0,
-    zIndex: 1000
+    zIndex: 1000,
+    boxShadow: '0px 3px 10px var(--test-9)'
+    // boxShadow: 0px 3px 10px #F1F9DC;
   }}>
     <div
       style={{

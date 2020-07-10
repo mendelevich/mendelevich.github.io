77d671b40078b9f21e0bb35bc67023176e62bcc9
Eve Mendelevich <ym9382@gmail.com>
2020-07-09 23:12:08 -0400

    added new image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,34 @@
 import React from "react"
-
-import Image from "./image"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 const Hero = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      flower: file(relativePath: { eq: "hero-img.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      cafe: file(relativePath: { eq: "cafe.jpeg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <div className="hero section">
       <div className="section-container">
         <h1>Welcome to my personal site!</h1>
-        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-          <Image />
-        </div>
+        <Img fluid={data.flower.childImageSharp.fluid} />
+        <Img fluid={data.cafe.childImageSharp.fluid} />
+        {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}> */}
       </div>
     </div>
   )

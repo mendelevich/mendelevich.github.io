2db31a8b059d182391f10e102d2e895f180c871c
Eve Mendelevich <ym9382@gmail.com>
2020-07-09 22:37:45 -0400

    added new palette

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,8 +4,8 @@ import Image from "./image"
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="hero-container">
+    <div className="hero section">
+      <div className="section-container">
         <h1>Welcome to my personal site!</h1>
         <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
           <Image />
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,17 +2,8 @@ import React from "react"
 
 const Projects = () => {
   return (
-    <div style={{
-      height: '100vh',
-    }}>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-
-        }}
-      >
+    <div className="projects section">
+      <div className="section-container">
         <h1>Projects</h1>
         {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
